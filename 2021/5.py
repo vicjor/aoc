51428c3fe3315faaf17db8000819e5f98c60a72c
Victor Jørgensen <victor.jorgens@gmail.com>
2021-12-05 16:42:49 +0100

    Day 5 complete.

--- 2021/5.py
+++ 2021/5.py
@@ -1,12 +1,7 @@
 from collections import defaultdict
 from time import perf_counter
 from utils.filereader import file_reader, file_reader_int
-from typing import List
-
-
-def is_vertical_or_horizontal(x1, y1, x2, y2):
-    """687,692 -> 57,692"""
-    return x1 == x2 or y1 == y2
+from typing import List, Tuple
 
 
 def is_vertical(x1: int, x2: int) -> bool:
@@ -18,22 +13,18 @@ def is_horizontal(y1: int, y2: int) -> bool:
 
 
 def is_diagonal(x1: int, y1: int, x2: int, y2: int) -> bool:
-    if is_horizontal(y1, y2):
-        return False
-    if is_vertical(x1, x2):
-        return False
-    return True
+    return not is_vertical(x1, x2) and not is_horizontal(y1, y2)
 
 
-def line_to_coordinates(line):
-    """941,230 -> 322,849"""
+def line_to_coordinates(line) -> Tuple[int, int, int, int]:
     x1y1, x2y2 = line.split("->")[0], line.split("->")[1]
     x1, y1 = int(x1y1.split(",")[0]), int(x1y1.split(",")[1])
     x2, y2 = int(x2y2.split(",")[0]), int(x2y2.split(",")[1])
-    return x1, y1, x2, y2
+    return (x1, y1, x2, y2)
 
 
 def aggregate_overlaps(lines, include_diagonals=False):
+    """Aggregate all points in grid crossed by lines."""
     coordinates = defaultdict(int)
     for line in lines:
         x1, y1, x2, y2 = line_to_coordinates(line)
@@ -53,22 +44,21 @@ def aggregate_overlaps(lines, include_diagonals=False):
                     coordinates[(x1, i)] += 1
         elif include_diagonals and is_diagonal(x1, y1, x2, y2):
             if x1 > x2 and y1 > y2:
-                for i in range(x2, x1 + 1):
-                    for j in range(y2, y1 + 1):
-                        coordinates[(i, j)] += 1
+                for i in range(x1-x2+1):
+                    # for j in range(y1-y2):
+                    coordinates[(x1-i, y1-i)] += 1
             elif x1 < x2 and y1 < y2:
-                for i in range(x1, x2 + 1):
-                    for j in range(y1, y2 + 1):
-                        coordinates[(i, j)] += 1
+                for i in range(x2-x1+1):
+                    # for j in range(y1, y2 + 1):
+                    coordinates[(x1+i, y1+i)] += 1
             elif x1 > x2 and y1 < y2:
-                for i in range(x2, x1 + 1):
-                    for j in range(y1, y2 + 1):
-                        coordinates[(i, j)] += 1
+                for i in range(x1-x2+1):
+                    # for j in range(y1, y2 + 1):
+                    coordinates[(x1-i, y1+i)] += 1
             elif x1 < x2 and y1 > y2:
-                for i in range(x1, x2 + 1):
-                    for j in range(y2, y1 + 1):
-                        coordinates[(i, j)] += 1
-    print(coordinates)
+                for i in range(x2-x1+1):
+                    # for j in range(y2, y1 + 1):
+                    coordinates[(x1+i, y1-i)] += 1
     return coordinates
 
 
@@ -83,16 +73,17 @@ def count_overlaps(coordinates: defaultdict(int)) -> int:
 def main():
     start = perf_counter()  # Start time
 
-    lines = file_reader("data/test5.txt")
-    coordinates = aggregate_overlaps(lines, True)
+    lines = file_reader("data/input5.txt")
+    coordinates = aggregate_overlaps(lines)
+    coordinates_2 = aggregate_overlaps(lines, True)
     overlaps = count_overlaps(coordinates)
+    overlaps_2 = count_overlaps(coordinates_2)
     print(f"Number of points with minimum two overlaps: {overlaps}")  # 5585
-    # 948637 too high
-
-    print(is_diagonal(941, 230, 322, 849))
+    print(
+        f"Number of points with minimum two overlaps including diagonals: {overlaps_2}")  # 17193
     end = perf_counter()  # End time
     time = end-start  # Total runtime
-    print(f"Total runtime: {time} seconds.")
+    print(f"\nTotal runtime: {time} seconds.")
 
 
 if __name__ == "__main__":
